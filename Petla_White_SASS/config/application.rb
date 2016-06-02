48d6f5bcd3ea9e479ccf42b9af867f7ebedce816
ThomasDebek <tom.debek@gmail.com>
2016-06-02 21:17:43 +0200

    wyzucenie generowania zbednych rzeczy

--- Petla_White_SASS/config/application.rb
+++ Petla_White_SASS/config/application.rb
@@ -22,5 +22,13 @@ module PetlaWhiteSass
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.generators do |g|
+      g.assets = false
+      g.helper = false
+      g.view_specs = false
+    end
+
+
   end
 end
